1dc4b6bc74d019277b8e61f38e06d0c628337a3f
unknown <Administrator@.(none)>
2010-01-25 14:12:55 -0500

    Initial commit

---
+++ CapstoneWeatherDashboard/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneWeatherDashboard.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "It rained yesterday.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ CapstoneWeatherDashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CapstoneWeatherDashboard.MvcApplication" Language="C#" %>
