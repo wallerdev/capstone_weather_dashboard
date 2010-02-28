0241cb917c5decc960143e195b67b866305f273f
Edward Waller <edward@wallerdev.com>
2010-02-27 23:08:18 -0500

    Brought back the zip code lookup to speed up geocoding for simple zip code searches

---
+++ WeatherStation/ZipCodeLookup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace WeatherStation
+{
+    /// <summary>
+    /// Looks up relavent information based on zipcode.
+    /// Source: http://www.populardata.com/zipcode_database.html
+    /// </summary>
+    public class ZipCodeLookup
+    {
+        readonly IEnumerable<ZipCodeLookupEntry> _zipCodeLookupEntries;
+
+        public ZipCodeLookup()
+        {
+            IEnumerable<string> entries = Properties.Resources.ZipCodeTable.Split(new[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+            _zipCodeLookupEntries = entries.Select(entry =>
+                new ZipCodeLookupEntry(entry.Split(',').Select(data => data.Trim('"')).ToList()));
+        }
+
+        public string GetCounty(string zipCode)
+        {
+            return GetEntry(zipCode).County;
+        }
+
+        public string GetState(string zipCode)
+        {
+            return GetEntry(zipCode).State;
+        }
+
+        public string GetCity(string zipCode)
+        {
+            return GetEntry(zipCode).City;
+        }
+
+        ZipCodeLookupEntry GetEntry(string zipCode)
+        {
+            return _zipCodeLookupEntries.First(e => e.ZipCode == zipCode);
+        }
+    }
+}
+
+
